1859bf921b04b2b719df5c253f2b32ec2efae8be
Ntihanab <ntihanabayoeric2@gmail.com>
2022-03-29 10:21:18 -0700

    eric

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,15 +1,20 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#include "main.h"
 
-int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
+/**
+ * _memset - concatenates n bytes of two strings
+ * @s: arg 1
+ * @b: arg 2
+ * @n: arg 3
+ * Return: char
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+    int i;
 
-#endif
+    for(i = 0; i < n;i++)
+    {
+        *s = b;
+        s++;
+    }
+    return s;
+}
